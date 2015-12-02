790b78386ea13966548ae6a578ea48d4092ac728
Anamarija Mijatović <anamarija995m@gmail.com>
2015-12-02 12:05:25 +0100

    uvoz podatkov, brisanje stolpcev, preiimenovanje stolpcev

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,20 +1,50 @@
 # 2. faza: Uvoz podatkov
 
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+uvozi.zaposlenost <- function() {
+  return(read.table("podatki/brezposlenost.csv", sep = ";", as.is = TRUE,header = TRUE,
+                      fileEncoding = "utf-8"))
 }
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine()
 
-obcine <- uvozi.obcine()
+uvozi.vstop <- function() {
+  return(read.table("podatki/vstop.csv", sep = ",", as.is = TRUE,header = TRUE,
+                    fileEncoding = "utf-8"))
+}
+
+
+uvozi.BDP <- read.xlsx(file = "podatki/bdp.xlsx", sheetName = "ObservationData",header=TRUE)
+
+
+# Zapišimo podatke v razpredelnico druzine.
+zaposlenost <- uvozi.zaposlenost()
+
+vstop <- uvozi.vstop()
+
+BDP <- uvozi.BDP()
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
 # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
 # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
 # fazah.
+#Urejanje tabel
+vstop$variable <- NULL
+vstop$Unit <- NULL
+View(vstop)
+
+zaposlenost2<- zaposlenost[c("Country.Name","X2015")]
+bdp2<- uvozi.BDP[c("Country.Name","X2015")]
+
+names(vstop)[1] <- "Država"
+names(vstop)[2] <- "Datum"
+names(vstop)[3] <- "Število tujcev, ki so vstopili v državo"
+Encoding(names(vstop))<-"WINDOWS-1250"
+
+names(zaposlenost2)[1] <- "Država"
+names(zaposlenost2)[2] <- "Število zaposlenih v turizmu(%)"
+Encoding(names(zaposlenost2))<-"WINDOWS-1250"
+
+names(bdp2)[1] <- "Država"
+names(bdp2)[2] <- "Delež BDP-ja ki ga predstavlja turizem (%)"
+Encoding(names(zaposlenost2))<-"WINDOWS-1250"
